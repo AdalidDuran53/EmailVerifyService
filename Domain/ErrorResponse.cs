a1f1ca86c222290616de9a442eaa7346f6bfa8b9
Adalid Duran Calderon <adalid.duran@outlook.com>
2026-02-06 12:39:42 -0600

    EVS-5 add SwaggerResponseExample

---
+++ Domain/ErrorResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class ErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string? Details { get; set; }
+    }
+
+}
--- EmailVerifyService/Controllers/EmailVerifyControllerBase.cs
+++ EmailVerifyService/Controllers/EmailVerifyControllerBase.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmailVerifyService.Filters;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
 using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.Filters;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace EmailVerifyService.Controllers
@@ -14,19 +17,29 @@ namespace EmailVerifyService.Controllers
         [EnableRateLimiting("IpPolicy")]
         [Route("~/{version}/EmailVerify/")]
         [SwaggerOperation(OperationId = "RequestVerifyCode")]
-        [SwaggerResponse(statusCode: 201, type: typeof(ActionResult), description: "Created")]
-        [SwaggerResponse(statusCode: 400, type: typeof(ActionResult), description: "Bab Request")]
-        [SwaggerResponse(statusCode: 401, type: typeof(ActionResult), description: "Unauthorized")]
-        public abstract Task<IActionResult> RequestVerifyCode([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required, EmailAddress] string emailAddress, Guid? appToken);
+        [SwaggerResponse(statusCode: StatusCodes.Status201Created, type: typeof(ActionResult), description: "Created")]
+        [SwaggerResponseExample(StatusCodes.Status201Created, typeof(CustomResponseCreatedExample))]
+        [SwaggerResponse(statusCode: StatusCodes.Status400BadRequest, type: typeof(ActionResult), description: "Bab Request")]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(CustomResponseBadRequestExample))]
+        [SwaggerResponse(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ActionResult), description: "Unauthorized")][SwaggerRequestExample(typeof(object), typeof(RequestVerifyCodeExample))]
+        public abstract Task<IActionResult> RequestVerifyCode(
+            [FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")][DefaultValue("0.1")] string version, 
+            [Required, EmailAddress] string emailAddress, 
+            Guid? appToken);
 
         [HttpPost]
         [EnableRateLimiting("IpPolicy")]
         [Route("~/{version}/EmailVerify/")]
         [SwaggerOperation(OperationId = "ValidateVerifyCode")]
-        [SwaggerResponse(statusCode: 200, type: typeof(ActionResult), description: "Ok")]
-        [SwaggerResponse(statusCode: 400, type: typeof(ActionResult), description: "Bab Request")]
-        [SwaggerResponse(statusCode: 401, type: typeof(ActionResult), description: "Unauthorized")]
-        public abstract Task<IActionResult> ValidateVerifyCode([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required, EmailAddress] string emailAddress, [Required] Guid token, [Required] string verifyCode );
+        [SwaggerResponse(statusCode: StatusCodes.Status200OK, type: typeof(ActionResult), description: "Ok")]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(CustomResponseOKExample))]
+        [SwaggerResponse(statusCode: StatusCodes.Status400BadRequest, type: typeof(ActionResult), description: "Bab Request")]
+        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(CustomResponseBadRequestExample))]
+        [SwaggerResponse(statusCode: StatusCodes.Status401Unauthorized, type: typeof(ActionResult), description: "Unauthorized")]
+        public abstract Task<IActionResult> ValidateVerifyCode(
+            [FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")][DefaultValue("0.1")] string version, 
+            [Required, EmailAddress] string emailAddress, 
+            [Required] Guid token, [Required] string verifyCode );
 
     }
 }
